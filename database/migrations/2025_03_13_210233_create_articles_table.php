2c7565d73568817e67fc2c821f5ba0e330b5fada
Toshine <toshineluv99@yahoo.com>
2025-03-14 21:06:36 +0100

    adding articles table migration

---
+++ database/migrations/2025_03_13_210233_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('source_code')->unique();
+            $table->string('author')->nullable();
+            $table->text('description')->nullable();
+            $table->text('content');
+            $table->string('url');
+            $table->string('source');
+            $table->string('category')->nullable();
+            $table->string('image_url')->nullable();
+            $table->timestamp('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
